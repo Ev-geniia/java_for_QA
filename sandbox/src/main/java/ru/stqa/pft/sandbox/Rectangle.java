c9d4b223d7833ad328f99d1ce36f8a2663118c5d
Ev-geniia <moonlitcat@inbox.ru>
2021-01-17 20:39:51 +0300

    new classes added

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class Rectangle {
+  public double l;
+  public double w;
+
+  public Rectangle(double l, double w) {
+    this.l = l;
+    this.w = w;
+  }
+}
